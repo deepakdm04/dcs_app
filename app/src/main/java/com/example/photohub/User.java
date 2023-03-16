2e9580fb762d4dd93cc8c1f42ff8fa489c68fad1
deepakdm04 <125183679+deepakdm04@users.noreply.github.com>
2023-03-16 13:36:52 +0530

    Update User.java

--- app/src/main/java/com/example/photohub/User.java
+++ app/src/main/java/com/example/photohub/User.java
@@ -1,17 +1,47 @@
 package com.example.photohub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Calendar;
 
 public class User extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+
+    private TextView banner,registerUser;
+    private EditText editTextfullname, editTextage,editTextemail,editTextpassword;
+    private Button register;
+    private Calendar FirebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        banner = (TextView) findViewById(R.id.banner);
+banner.setOnClickListener(this);
+
+registerUser =(Button) findViewById(R.id.register);
+registerUser.setOnClickListener(this);
+
+        editTextfullname = (EditText) findViewById(R.id.fullname);
+        editTextage = (EditText) findViewById(R.id.age);
+        editTextemail = (EditText) findViewById(R.id.email);
+        editTextpassword = (EditText) findViewById(R.id.password);
+
     }
 
 
@@ -20,4 +50,41 @@ public class User extends AppCompatActivity implements View.OnClickListener {
         Intent i = new Intent(User.this,Register.class);
         startActivity(i);
     }
+    private void registerUser()
+    {
+        String email = editTextemail.getText().toString().trim();
+        String password = editTextpassword.getText().toString().trim();
+        String fullName = editTextfullname.getText().toString().trim();
+        String age = editTextage.getText().toString().trim();
+
+        if(fullName.isEmpty())
+        {
+            editTextfullname.setError("FULL NAME IS REQUIRED");
+            editTextfullname.requestFocus();
+            return;
+        }
+        if(age.isEmpty()) {
+            editTextage.setError("AGE IS REQUIRED");
+            editTextage.requestFocus();
+            return;
+        }
+        if(email.isEmpty())
+        {
+            editTextemail.setError("EMAIL IS MANDATORY");
+            editTextemail.requestFocus();
+            return;
     }
+       if (password.isEmpty())
+       {
+           editTextpassword.setError("PASSWORD IS REQUIRED!");
+           editTextpassword.requestFocus();
+       }
+       if (password.length()<8) {
+           editTextpassword.setError("MIN PASSWORD LENGTH SHOULD BE 8 CHARACTERS");
+           editTextpassword.requestFocus();
+           return;
+       }
+
+       mAuth.createUserWithEmailAndPassword(email,password);
+       }
+        }
